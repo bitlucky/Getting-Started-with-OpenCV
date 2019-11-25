7f6dde81252a657fbfe068ac96f44b71ee813ec5
bitlucky <lokeshtiwari877@gmail.com>
2019-11-26 02:13:56 +0530

    Initial commit

--- getting_started_with_videos.py
+++ getting_started_with_videos.py
@@ -1,14 +1,26 @@
 import cv2
-cap=cv2.VideoCapture(0);
 
-while(True):
+cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  #set video codecs using fourcc
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480)) #four arguements in total filename, codec, framerate , pixels
+
+print(cap.isOpened())
+while(cap.isOpened()):   #check for if frame is opened 
     ret, frame = cap.read()
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('frame', gray)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if ret == True:
+       print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  #get frame height and width
+       print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) 
+
+       out.write(frame)  #same as dev.off() write file in frames
+
+       gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  #convert original frame filter to Grey Scale
+       cv2.imshow('frame', gray)
+
+       if cv2.waitKey(1) & 0xFF == ord('q'): #wait for key q to be pressed
+         break
+    else:
         break
-    
+
 cap.release()
+out.release()
 cv2.destroyAllWindows()
